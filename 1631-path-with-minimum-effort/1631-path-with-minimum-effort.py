d937f3d31ab9566ec55a9fa91075c728ea945ef0
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-07-07 06:14:34 +0530

    Time: 253 ms (60.86%), Space: 21.9 MB (29.14%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -3,57 +3,36 @@ import heapq
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         row,col = len(heights) ,len(heights[0])
-        
-        graph = [[float("inf")]*col for i in range(row)]
 
-        graph[0][0] = 0
+        min_heap =  [(0,0,0)]
 
-        min_heap = [(0,0,0)]
+        visit = set()
+
+        directions = [(0,-1),(0,1),(-1,0),(1,0)]
 
         while min_heap:
-                
-            abs_dif,r,c = heapq.heappop(min_heap)
 
-            curr = abs_dif
+            diff,r,c = heapq.heappop(min_heap)
 
-            if r== row-1 and c== col-1:
-                return abs_dif
-
-            if abs_dif > graph[r][c]:
+            if (r,c) in visit:
                 continue
 
-            # left
-            if c >0:
-                new_effort = max(curr , abs(heights[r][c] - heights[r][c-1]))
-                if new_effort < graph[r][c-1]:
-                    graph[r][c-1] = new_effort
-                    heapq.heappush(min_heap,(new_effort,r,c-1))
-                    
+            visit.add((r,c))
 
-            # right
-            if c<col-1:
-                new_effort = max(curr , abs(heights[r][c] - heights[r][c+1]))
-                if new_effort< graph[r][c+1]:
-                    graph[r][c+1] = new_effort
-                    heapq.heappush(min_heap,(new_effort,r,c+1))
-                
-            # up
-            if r>0:
-                new_effort= max(curr , abs(heights[r][c] - heights[r-1][c]))
-                if new_effort < graph[r-1][c]:
-                    graph[r-1][c] = new_effort
-                    heapq.heappush(min_heap,(new_effort,r-1,c))
+            if (r,c) == (row-1,col-1):
+                return diff
 
-            # down
-            if r<row-1:
-                new_effort = max(curr , abs(heights[r][c] - heights[r+1][c]))
-                if new_effort < graph[r+1][c]:
-                    graph[r+1][c] = new_effort
-                    heapq.heappush(min_heap,(new_effort,r+1,c))
+            for dr , dc in directions:
 
-        return 0
-                
+                nr , nc = r+dr , c+dc   # new r , new c
 
+                if (nr<0 or nc< 0 or nr==row or nc == col or (nr,nc) in visit):
+                    continue
+
+                new_diff = max(diff, abs( heights[r][c]-heights[nr][nc] ))
+                heapq.heappush(min_heap,(new_diff,nr,nc))
+
+            
 
 
 
