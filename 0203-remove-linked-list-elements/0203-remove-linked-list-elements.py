02f1d939682b6bac468a03495169a7eb9eefbf0c
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-07-15 12:52:06 +0530

    Time: 0 ms (100%), Space: 22.4 MB (66.93%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -7,26 +7,28 @@ class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         if head is None:
             return head
-
+        # if all are vals like (7 --> 7 --> 7)
         while head and head.val==val:
             head = head.next
 
         if head is None :
             return head
 
-        prev = head
-        curr = head.next
+        dummy = ListNode(0)
+        curr1 = dummy
+        curr = head
 
         while curr :   
-            if curr.val == val:
-                prev.next = curr.next
+            if curr.val != val:
+                curr1.next = curr
+                curr1 = curr1.next
                 curr = curr.next
-                continue
+            else:
+                curr1.next = curr.next
+              
+                curr = curr.next
 
-            prev = curr
-            curr = curr.next
-
-        return head
+        return dummy.next
 
                         
 
