b59267e21c69680a65869b37442aea465da82d41
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-07-21 12:26:59 +0530

    Time: 27 ms (5.7%), Space: 19.4 MB (88.26%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -33,4 +33,24 @@ class Solution:
         return max_lenght
 
 
+        #  or 
+        # def valid_sub_string(s):
+            # if s == ")":
+            #     return 0
+            # stk =[-1]
+            # max_lenght = 0
+            # for i in range(len(s)):
+            #     if s[i] in "(":
+            #         stk.append(i)
+
+            #     else:
+            #         stk.pop()
+            #         if not stk:
+            #             stk.append(i)
+            #         max_lenght = max(max_lenght, i - stk[-1])
+
+
+            # return max_lenght
+
+
         
