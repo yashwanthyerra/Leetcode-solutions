069834854f92d81f2ae94f817d98b2cbab5c88cf
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-07-31 19:27:01 +0530

    Time: 0 ms (100%), Space: 19.7 MB (13.94%) - LeetHub

---
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        needs_cameras =0
+        has_cameras =1
+        covered = 2
+        placed_cameras = 0
+        
+        def dfs(root):
+           
+
+            nonlocal placed_cameras
+
+            if not root :
+                return covered
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+
+
+            if left ==needs_cameras or right ==needs_cameras:
+                placed_cameras+=1
+                return has_cameras
+
+            if left==has_cameras or right==has_cameras:
+                return covered
+            
+            return needs_cameras
+
+
+        if dfs(root)==needs_cameras:
+            placed_cameras+=1
+
+        return placed_cameras
+
+
+
+
+        
+
+        
