e90136d0b0baf8af36120dde9223ef1f0443c61f
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-08-04 07:33:57 +0530

    Time: 355 ms (17.42%), Space: 137.6 MB (13.22%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,4 +1,16 @@
+from functools import cache
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        @cache
+        def dp(i, j):
+          
+            if i == j:
+                return piles[i]
+
+            takeLeft = piles[i] - dp(i + 1, j)
+            takeRight = piles[j] - dp(i, j - 1)
+
+            return max(takeLeft, takeRight)
+
+        return dp(0, len(piles) - 1) > 0
         
