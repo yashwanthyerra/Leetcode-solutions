288c9e9fcf84491d7cf656d54aad970672e201ed
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-08-05 10:51:30 +0530

    Time: 1938 ms (9.27%), Space: 25.6 MB (5.65%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -1,10 +1,3 @@
-
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 def insert(root,key):
     if root is None:
         return TreeNode(key)
@@ -24,8 +17,6 @@ def same_tree(a,b):
     return (a.val==b.val and same_tree(a.left,b.left) and same_tree(a.right,b.right))
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-
-
         result = []
         arr = [i for i in range(1,n+1)]
         x  = list(permutations(arr))
@@ -41,8 +32,6 @@ class Solution:
                     break
             if is_new:
                 result.append(root)
-
-
         return result
 
 
