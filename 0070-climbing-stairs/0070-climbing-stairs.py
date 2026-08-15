2c43eaca80d66fbd256c47c3404d9e91a97180f9
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-08-15 17:10:35 +0530

    Time: 3 ms (2%), Space: 19.4 MB (17.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,14 @@
-def fac(y):
-    if y==1 or y==0:
-        return 1
-    return y * fac(y-1)
-
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==1 :
-            return 1
 
-        count = 1
+        memo = {1:1,2:2}
 
-        
-        for i in range(1,n//2 +1):
-            count += fac(n-i)//((fac(i))*(fac(n-(2*i))))
-
-        
-
-        return count
-
-
-        
+        def climb(n):
+            
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = climb(n-1) + climb(n-2)
+                return memo[n]
+        return  climb(n)
         
