1829ac4014e1cd3fa809b67e8cd8d5dfcb15dccf
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-08-18 19:57:31 +0530

    Time: 12 ms (30.43%), Space: 21.1 MB (48.6%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,9 +1,7 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         l_wall = r_wall = 0
-
         n = len(height)
-
         max_left = [0]*n        
         max_right = [0]*n
 
@@ -17,11 +15,10 @@ class Solution:
             r_wall = max(r_wall,height[j]) 
 
         rain_trapped = 0
-
+        
         for i in range(n):
             potential= min(max_left[i],max_right[i])
             rain_trapped += max(0,potential-height[i])    
-
         return rain_trapped
 
 
