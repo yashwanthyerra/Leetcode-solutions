26a868e86f623d365ec8a29a4e4702fbc3717a00
yashwanthyerra <yashwanthyerra2007@gmail.com>
2026-08-21 00:12:10 +0530

    Time: 864 ms (19.35%), Space: 24 MB (5.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,35 +7,25 @@ class Solution:
         for i in range(n):
             if i>0 and nums[i] == nums[i-1]:
                 continue
-            left = i+1
+            left  = i+1
             right = n-1
-        
-
-            while left<right:
-
-                
-
-                if nums[left] + nums[right] == -nums[i] :
+            while left < right:
+                if nums[left] + nums[right] == -nums[i]:
                     l=[]
                     l.append(nums[left])
                     l.append(nums[right])
                     l.append(nums[i])
-                    l= sorted(l)
-                  
-                    
+                    l = sorted(l)
                     key = tuple(l)
-
                     if key not in used:
                         result.append(l)
                         used[key] = True
-
-                    left += 1
-                    right -= 1
-
-                elif nums[left] + nums[right] < -nums[i]:
-                    left  += 1
+                    left+=1
+                    right-=1
+                elif  nums[left] + nums[right] < -nums[i]:
+                    left+=1
                 else:
-                    right -= 1
-            
+                    right-=1
         return result
                 
+       
